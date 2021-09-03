618996576b84b6a4a8ee76e875dd14e46b1c441c
Rene <rdejramirez@gmail.com>
2021-09-02 22:03:27 -0500

    conectado a mysql desde heroku

--- conexion.php
+++ conexion.php
@@ -9,7 +9,8 @@ class BD
         if (!isset(self::$instancia)) {
             $opcionesPDO[PDO::ATTR_ERRMODE]= PDO::ERRMODE_EXCEPTION;
 
-            self::$instancia = new PDO('mysql:host=localhost;dbname=empleados', 'root', '', $opcionesPDO);
+            //self::$instancia = new PDO('mysql:host=localhost;dbname=empleados', 'root', '', $opcionesPDO);
+            self::$instancia = new PDO('mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_8d9e722ab58ceb6', 'b0526ccb9ea09d', '337b8657', $opcionesPDO);
             //echo "conexion realizada";
         }
         return self::$instancia;
